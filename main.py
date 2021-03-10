153667be9b0c3f303478cddd1e9d6f8fc33ed92c
repl.it user <replituser@example.com>
2021-03-10 06:16:42 +0000

    Nested decisions task3

---
+++ main.py
@@ -0,0 +1,23 @@
+#Beep asks for his battery
+print("Have you seen my spare battery?")
+answer=input()
+if (answer=="yes"):
+  print("Where should I look:")
+  answer=input()
+  if (answer=="in the bedroom"):
+    print("Where in the bedroom should I look?")
+    answer=input()
+    if ("answer==under the bed"):
+      print("I found some trash but not my battery!")
+    else:
+      print("I do not know wher to find my battery!")
+  if (answer=="in the bathroom"):
+    print("Where in the bathroom should I look?")
+    answer=input()
+    if (answer=="in the bathtub"):
+      print("I found a condom but not my battery!")
+    else:
+      print("I found some wet surfaces but not my battery!")
+else:
+  print("I will look for the battery without your help, human!")
+           
